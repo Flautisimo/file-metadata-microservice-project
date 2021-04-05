215ea5a1990c4e9879d9c6b8301c39742dd35051
Glitch (pretty-equal-ceder) <none>
2021-04-05 11:15:37 +0300

    This is my first commit message

---
+++ .bash_history
@@ -0,0 +1,3 @@
+npm intall multer
+npm install multer
+refresh
--- server.js
+++ server.js
@@ -1,28 +1,27 @@
-var express = require('express');
-var cors = require('cors');
-var multer = require('multer');
-require('dotenv').config()
+var express = require("express");
+var cors = require("cors");
+var multer = require("multer");
+require("dotenv").config();
 
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function(req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-app.post('/api/fileanalyse', multer().single('upfile'), (req, res) => {
+app.post("/api/fileanalyse", multer().single("upfile"), (req, res) => {
   let responseObject = {};
-  responseObject['name'] = req.file.originalname;
-  responseObject['type'] = req.file.mimetype;
-  responseObject['size'] = req.file.size;
-  
-  res.json(responseObject);
-})
+  responseObject["name"] = req.file.originalname;
+  responseObject["type"] = req.file.mimetype;
+  responseObject["size"] = req.file.size;
 
+  res.json(responseObject);
+});
 
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+app.listen(port, function() {
+  console.log("Your app is listening on port " + port);
 });
